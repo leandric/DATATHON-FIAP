d62c4d52545b6afbe163cf8cc7ba059909c94b60
Leandro Silva <leandric@Leandros-MacBook-Pro.local>
2024-09-23 23:10:38 -0300

    ajuste

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
 import missingno as msno
 import matplotlib.pyplot as plt
 
